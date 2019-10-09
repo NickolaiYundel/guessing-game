3379366f28a56bfe978822b22d4cf3f5be64a4cb
qweer <stalkeerr44@gmail.com>
2019-10-09 23:42:44 +0300

    implement function GuessingGame

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,31 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.guessResult = null;
+        this.maxNumber = null;
+        this.minNumber = null;
+    }
 
     setRange(min, max) {
-
+        this.maxNumber = max; 4048
+        this.minNumber = min; 0
+        this.guessResult = this.maxNumber / 2; 2024
     }
 
     guess() {
 
+        return (this.guessResult);
     }
 
     lower() {
 
+        this.maxNumber = this.guessResult;
+        this.guessResult = Math.ceil(this.guessResult - (this.maxNumber - this.minNumber) / 2);
+
     }
 
     greater() {
-
+        this.minNumber = this.guessResult;
+        this.guessResult = Math.ceil(this.guessResult + (this.maxNumber - this.minNumber) / 2);
     }
 }
 
